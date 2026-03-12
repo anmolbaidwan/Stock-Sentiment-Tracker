4794a9b12f716c94176257c7f36126f400fbf6ac
anmolbaidwan <anmolbaidwan@gmail.com>
2026-03-11 17:22:47 -0700

    added search date to api_client
    searchDate is todays date - 6 months to get as many articles as possible from that date

--- src/api_client.py
+++ src/api_client.py
@@ -2,6 +2,8 @@ import os
 import time
 import requests
 from dotenv import load_dotenv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 load_dotenv()
 
@@ -9,18 +11,19 @@ class AlphaVantageClient:
     def __init__(self):
         self.api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
         self.base_url = "https://www.alphavantage.co/query"
+        past = datetime.now() + relativedelta(months=-6)
+        self.searchDate = past.strftime('%Y%m%d') + "T0000"
 
-    def _call_api(self, params):
+    def call_api(self, params):
         #helper to handle the 1-second delay and the request.
         response = requests.get(self.base_url, params=params)
         time.sleep(1.1) 
-        
         return response.json()
 
     def get_stock_price(self, symbol):
-        params = {"function": "GLOBAL_QUOTE", "symbol": symbol, "apikey": self.api_key}
-        return self._call_api(params)
+        params = {"function": "TIME_SERIES_DAILY", "symbol": symbol, "outputsize":"compact", "apikey": self.api_key}
+        return self.call_api(params)
 
     def get_sentiment(self, symbol):
-        params = {"function": "NEWS_SENTIMENT", "tickers": symbol, "apikey": self.api_key}
-        return self._call_api(params)
+        params = {"function": "NEWS_SENTIMENT", "tickers": symbol, "apikey": self.api_key, "time_from": self.searchDate, "limit":1000}
+        return self.call_api(params)
