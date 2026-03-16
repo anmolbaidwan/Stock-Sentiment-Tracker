66789c5720efccd07fd73a36b748ce8fd02715ac
anmolbaidwan <anmolbaidwan@gmail.com>
2026-03-16 12:32:34 -0700

    Update build.py

--- src/build.py
+++ src/build.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from api_client import AlphaVantageClient
 
 def buildDataset(price_dict, sentiment_dict, ticker):
-    dataset = []
+    dataset = {}
     dates = sorted(price_dict.keys()) #sort dates 
 
     for i in range(1, len(dates)): 
@@ -16,12 +16,11 @@ def buildDataset(price_dict, sentiment_dict, ticker):
 
         sentiment = sentiment_dict.get(today, 0) - sentiment_dict.get(yesterday, 0) #if sentiment doesnt exist for that day, set to 0 for now
 
-        dataset.append({ #add a dict with these values
-            "date": today,
+        dataset[today] = { #add a dict with these values
             "price": today_price,
             "sentiment": sentiment,
             "return": daily_return,
-        })
+        }
 
         for i in range(len(dataset) - 1): #afterwards, add a next_return which is just tomorrows return
             dataset[i]["next_return"] = dataset[i + 1]["return"]
